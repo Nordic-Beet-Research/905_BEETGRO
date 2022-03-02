f8101202028d73518b6d504b70c8b36bb673773f
Will English <80693248+meranbioph@users.noreply.github.com>
2022-03-02 17:00:00 +0100

    Update

--- 780_growth_model_v1.R
+++ 780_growth_model_v1.R
@@ -32,9 +32,8 @@
     "RPostgreSQL_0.7-3",
     "RPostgres_1.4.1",
     "ggplot2_3.3.5",
-    "plotrix_3.8-2",
-    "data.table_1.14.2",
-    "dplyr_1.0.7"
+    "dplyr_1.0.7",
+    "writexl_1.4.0"
   )
   
   path_Rpackages = "C:/R packages_412"
@@ -81,7 +80,7 @@ dbDisconnect(con)
 # TAKE DATA FROM JUST THE ONE YEAR
 
 dat <- weather_dat %>%
-  filter(year == 2021)
+  filter(year %in% c(2018, 2019, 2020, 2021, 2022))
 
 ############################################
 # SOME EXTRA STATION DATA
@@ -89,8 +88,8 @@ dat <- weather_dat %>%
 dat_stations <- data.frame("nr" = c(40141, 40142, 40143, 40144, 40145),
                            "lat" = c(55.88643,55.50544,55.37719,55.89354,55.77791),
                            "long" = c(14.03551, 14.07499, 13.40477, 12.9145510, 13.32169),
-                           "height" = c(10, 10, 10, 10, 10),
-                           "wind_height" = c(10 ,10 ,10 ,10, 10))
+                           "mamsl" = c(10, 10, 10, 10, 10),
+                           "wind_height" = c(2 ,2 ,2 ,2, 2))
 
 dat <- left_join(dat, dat_stations, by = "nr")
 
@@ -98,5 +97,75 @@ dat <- left_join(dat, dat_stations, by = "nr")
 # CALCULATE ET, AND THE STEPS ALONG THE WAY
 
 dat <- dat %>% 
-  mutate(wind_ground = xvh * 4.87/log(67.8*wind_height-5.42))
-         
+  mutate(
+    # WIND SPEED AT GROUND LEVEL
+    wind_ground = xvh * 4.87/log(67.8*wind_height-5.42),
+        
+    # SLOPE OF THE SATURATION PRESSURE CURVE
+    slopesat = 4098*(0.6108*exp(17.27*xtemp/(xtemp+237.3)))/(xtemp+237.3)^2,
+
+    # PRESSURE
+    press = 101.3*((293-0.0065*mamsl)/293)^5.26,
+
+    # PSYKROMETRIC CONSTANT
+    psychroC = 0.000665*press,
+
+    # DELTA
+    delta = slopesat/(slopesat+psychroC*(1+0.34*xvh)),
+
+    # PSI
+    psi = psychroC/(slopesat+psychroC*(1+0.34*xvh)),
+    
+    # TEMP TERM
+    tempt = 900*xvh/(xtemp+273),
+    
+    # SATURATION PRESSURE MAX
+    satPress_max = 0.6108*exp(17.27*htemp/(htemp+237.3)),
+    
+    # SATURATION PRESSURE MIN
+    satPress_min = 0.6108*exp(17.27*ltemp/(ltemp+237.3)),
+    
+    # VAPOUR PRESSURE
+    vapourPress = (satPress_max*hhum/100 + satPress_min*lhum/100)/2,
+    
+    # INVERTED SUN DISTANCE
+    invSun = 1 + 0.033*cos(2*pi/365*dagnr),
+    
+    # SUN DECLINATION
+    sunDecl = 0.409*sin(2*pi/365*dagnr - 1.39),
+    
+    # SUN ANGLE
+    sunAngle = acos(-tan(lat*pi/180)*tan(sunDecl)),
+    
+    # SPACE RADIATION
+    spaceRadiation = 24*60/pi*0.082*invSun*(sunAngle*sin(lat*pi/180)*sin(sunDecl)+cos(lat*pi/180)*cos(sunDecl)*sin(sunAngle)),
+    
+    # GLOBAL RADIATION
+    globalRadiation = (0.75+2*10^(-5)*mamsl)*spaceRadiation,
+    
+    # NET RADIATION
+    netRadiation = (1-0.23)*solin,
+    
+    # LONGTERM RADIATION
+    longtermRadiation = 4.903*10^(-9)*((ltemp+273.16)^4+(htemp+273.16)^4)/2*(0.34-0.14*vapourPress^0.5)*(1.35*solin/globalRadiation-0.35),
+    
+    # RADIATION BALANCE
+    radiationBalance = 0.408*(netRadiation - longtermRadiation),
+    
+    # EVAPOTRANSPIRATION
+    et = delta * radiationBalance + psi * tempt * ((satPress_max + satPress_min)/2 - vapourPress)
+  )
+
+############################################
+# WRITE EXCEL
+
+write_xlsx(dat, "dat.xlsx")
+
+############################################
+# # VISUALISATIONS
+# 
+# ggplot(data = dat[which(dat$nr == 40145),], aes(x=dagnr, y=et)) +
+#   geom_line()
+# 
+# ggplot(data = dat, aes(x=et)) +
+#   geom_histogram()
