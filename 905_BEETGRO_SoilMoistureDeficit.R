2d464c1c69c46057d3e41feab37a6354e4ac279d
Will English <80693248+meranbioph@users.noreply.github.com>
2023-01-17 21:01:14 +0100

    Just worked out that OpenModel is row-wise...

--- 905_BEETGRO_SoilMoistureDeficit.R
+++ 905_BEETGRO_SoilMoistureDeficit.R
@@ -78,43 +78,38 @@ dat_in_canopy <- read_xlsx("dat_canopy_2021.xlsx", sheet = "Sheet1")
 ############################################
 # CREATE FOUNDATION DATAFRAME
 
+i=7L
+
+dat_in_trial_i <- dat_in_trial[i,]
+
 ## Create base data frame for each day of the year
 smd_df <- data.frame(doy = seq(1:365),
-                     smd = 0) %>%  # browseURL("https://en.wikipedia.org/wiki/BBCH-scale_(beet)")
-
+                     smd = 20) %>%
 
 ## Join the weather data (NEED TO ADD IN )
   left_join(
     select(dat_in_weath, c(doy, et)), 
     by = "doy") %>% 
-  left_join(dat_in_canopy, by = "doy")
-
-i=7L
-
-dat_in_trial_i <- dat_in_trial[i,]
-
-## Update data frame with SMD = 1 at sowing and all dates thereafter
-smd_df <- smd_df %>% 
-  mutate(smd = replace(smd, bbch >= 01, 1))
+  left_join(dat_in_canopy, by = "doy") %>% 
 
 ############################################
 # SOIL MOISTURE DEFICIT
-
-## Calculate daily change
-### Preparations
-
-
-### Daily soil surface evaporation (SSE)
-smd_df <- smd_df %>%
-  mutate(dSSE = ifelse(f < 1, min(1.5, et)*(1-f), 0))
+## Update data frame with SMD = 1 at sowing and all dates thereafter
+  mutate(smd = replace(smd, bbch >= 01, 1),
+         
+         ### Daily soil surface evaporation (SSE)         
+         dSSE = ifelse(f < 1, min(1.5, et)*(1-f), 0))
 
 ### Ea Actual Crop Evapotranspiration
 smd_df <- smd_df %>%
-  mutate(Eatmos = 1.2*f*et,
+  rowwise() %>% 
+  mutate(smd = replace(smd, smd < 0, 0),
+         Eatmos = 1.2*f*et,
          Eatmos = replace(Eatmos, Eatmos <= 0, 0.001),
+         q = max(dat_in_trial_i$qfc - lag(smd)/(root_depth*1000), 0.01))
+,
+         
          Esoil = 2,
-         Ea = min(Eatmos, Esoil))
-
-#mutate
-dSMD <- dSSE + Ea - rain
-SMD <- lag(SMD) + dSMD
+         Ea = min(Eatmos, Esoil),
+         dSMD = dSSE + Ea - neda,
+         smd = lag(smd) + dsmd)
--- 905_BEETGRO_canopy.R
+++ 905_BEETGRO_canopy.R
@@ -47,7 +47,8 @@
     "dplyr_1.0.7",
     "tidyr_1.1.4",
     "readxl_1.3.1",
-    "writexl_1.4.0"
+    "writexl_1.4.0",
+    "lubridate_1.8.0"
   )
   
   path_Rpackages = "C:/R packages_412"
@@ -76,9 +77,11 @@
 # READ IN DATA
 
 # read in model parameters
-dat_in_param <- pivot_wider(read_xlsx("parameters.xlsx", sheet = "parameters"),names_from = parameter)
+param <- pivot_wider(read_xlsx("parameters.xlsx", sheet = "parameters"),names_from = parameter)
 # read in trial data
 dat_in_trial <- read_xlsx("TrialData.xlsx", sheet = "TrialData")
+# read in weather data
+dat_in_weath <- read_xlsx("dat_weather_2021_40141.xlsx", sheet = "Sheet1")
 # read which sites are in trail data, and read in appropriate weather data
 SiteIDs <- unique(dat_in_trial$SiteID)
 Sites <- data.frame("year" = numeric(),
@@ -124,12 +127,15 @@ pop3 <- ifelse(dat_in_trial_i$POP3 < 90000, -0.0003*(dat_in_trial_i$POP3/1000)^2
 poploss <- ifelse(dat_in_trial_i$PlantPop == 0, 1, mean(pop1, pop2, pop3))
 
 ## Soil parameters, function of b
+qfc <- param$a2 * (param$a1/5)^(1/b)
+qpwp <- param$a2 * (param$a1/1500)^(1/b)
+
 dat_in_trial_i <- dat_in_trial_i %>%
   mutate(
-    qfc = dat_in_param$a2 * (dat_in_param$a1/5)^(1/b),
-    qpwp = dat_in_param$a2 * (dat_in_param$a1/1500)^(1/b),
+    qfc = param$a2 * (param$a1/5)^(1/b),
+    qpwp = param$a2 * (param$a1/1500)^(1/b),
     q = qfc,                                      # ISSUE: THIS MEANS THAT QREL (NEXT LINE) ALWAYS = 1.
-    qrel = (q - qpwp) / (qfc - qpwp)
+    
   )
 
 ## Soil parameters, grouped by b
@@ -151,14 +157,14 @@ if(b > 20){
       WstressA = 2,
       WstressB = 0.6,
       WstressC = 300,
-      RUEZero1 = dat_in_param$RUEZero
+      RUEZero1 = param$RUEZero
   )
 }
 
 ############################################
 # TABLE OF EACH PARAMETER FOR EACH DAY OF YEAR FOR TRIAL "i".
 
-sow_date <- date(as.POSIXct(unlist(dat_in_trial[i,"SowDate"]), origin = '1970-01-01'))
+sow_date <- date(as.POSIXct(unlist(dat_in_trial_i["SowDate"]), origin = '1970-01-01'))
 emerg_date <- date(as.POSIXct(unlist(dat_in_trial[i,"EmDate"]), origin = '1970-01-01'))
 harvest_date <- date(as.POSIXct(unlist(dat_in_trial[i,"HarvestDate"]), origin = '1970-01-01'))
 sow_doy <-yday(sow_date)
@@ -166,7 +172,7 @@ emerg_doy <-yday(emerg_date)
 harvest_doy <-yday(harvest_date)
 
 can_i <- dat_in_weath %>%
-  select(doy, xtemp, neda
+  select(doy, xtemp, neda, et
          ) %>%
 
 # Crop status
@@ -179,48 +185,80 @@ can_i <- dat_in_weath %>%
 # Temperature
   mutate(
     #xtemp = (Tmax + Tmin)/2, # Called Temp in OpenModel
-    dT = xtemp - dat_in_param$Tbase,
+    dT = xtemp - param$Tbase,
     dT = replace(dT, dT < 0, 0)
     ) %>%
   group_by(sown = bbch >= 01) %>%
   mutate(
     Cd_sow = cumsum(dT),
     Cd_sow = replace(Cd_sow, bbch == 00, 0),
-    bbch = replace(bbch, dat_in_trial_i$EmDOY <= dat_in_trial_i$SowDOY & Cd_sow >= dat_in_param$Tzero, 01)
+    bbch = replace(bbch, dat_in_trial_i$EmDOY <= dat_in_trial_i$SowDOY & Cd_sow >= param$Tzero, 01)
   ) %>%
   group_by(emerged = bbch >= 09) %>%
   mutate(
     Cd_em = cumsum(dT) + 90,
     Cd_em = replace(Cd_em, bbch < 09, 0)
   ) %>%
-  ungroup %>% 
+  ungroup
 
-# Canopy
-  mutate(
-    f = dat_in_param$fZero,
-    WStress = 1,
-    WStress = replace(WStress, dat_in_trial_i$qrel < dat_in_trial_i$WstressB & xtemp > dat_in_trial_i$WstressC, 1),
-    T_Stress = (xtemp-3) * WStress, # Called DailyTemp in OpenModel
-    T_Stress = replace(T_Stress, xtemp < 3 | xtemp > 25 | bbch < 09, 0),
-    TotalTemp = cumsum(T_Stress),
-    TotalTemp = replace(TotalTemp, TotalTemp > 990, 990),
-    fTemp = TotalTemp * 0.001,
-    fTemp = replace(fTemp, fTemp < 0.00001, 0.00001),
-    f = 0.0015 + (0.99-0.0015)/(1+exp(-4*log((fTemp/(1-fTemp)))))
-    )
-
-# Canopy, with adjustments from user input (Pretty sure this isn't working properly in OpenModel)
-if(dat_in_trial_i$Canopy1 == 1 | dat_in_trial_i$Can1aCOV > 0){
-  Trial_i <- Trial_i %>%
+# GOING ROW WISE(). BUT THE PROBLEM IS THAT YOU CAN'T LAG IN ROWWISE(), SO HAVE TO GO TO LOOP
+for(j in 1:nrow(can_i)){
+  can_ij <- can_i[j,] %>%
     mutate(
-      f1 = dat_in_param$fZero,
+      # INITIAL CONDITIONS
+      
+      # CANOPY
+      f = param$fZero,
+      WStress = 1,
+      WStress = replace(WStress, qrel < dat_in_trial_i$WstressB & xtemp > dat_in_trial_i$WstressC, 1),
+      T_Stress = (xtemp-3) * WStress, # Called DailyTemp in OpenModel
+      T_Stress = replace(T_Stress, xtemp < 3 | xtemp > 25 | bbch < 09, 0),
       TotalTemp = cumsum(T_Stress),
       TotalTemp = replace(TotalTemp, TotalTemp > 990, 990),
       fTemp = TotalTemp * 0.001,
       fTemp = replace(fTemp, fTemp < 0.00001, 0.00001),
-      f = 0.0015 + (0.99-0.0015)/(1+exp(-4*log((fTemp/(1-fTemp)))))
-    )
-}
+      f = 0.0015 + (0.99-0.0015)/(1+exp(-4*log((fTemp/(1-fTemp))))),
+      
+      # ROOT DEPTH
+      root_depth = param$Dsowing + param$length0*exp(param$beta0/param$delta*(1-exp(-param$delta*(Cd_em-param$Tzero)))),
+      root_depth = replace(root_depth, bbch < 09, param$Dsowing),
+      
+      # WATER RELATIONS
+      smd = 20,
+      smd = replace(smd, doy == sow_doy, 0),
+      smd = replace(smd, smd < 0, 0),
+      dSSE = ifelse(f < 1, min(1.5, et)*(1-f), 0),
+      eatmos = 1.2*f*et,
+      eatmos = replace(eatmos, eatmos <= 0, 0.001),
+      q = max(qfc - smd/(root_depth*1000), 0.01),
+      qrel = q/qfc,
+      psiSoil = -5*(qrel)^(-b),
+      rsum = param$c1 + param$c2/root_depth*((qrel)^(-(2*b+3))-1)
+      )
+  
+  if(j==1) can_j <- can_ij
+  if(j!=1) can_j <- bind_rows(can_j, can_ij)
+  }
+
+can_i_names <- names(can_i)
+can_i_names <- can_i_names[-1]
+can_j <- can_j[,-which(names(can_j) %in% can_i_names)]
+
+can_i <- can_i %>% 
+  left_join(can_j, by ="doy")
+
+# # Canopy, with adjustments from user input (Pretty sure this isn't working properly in OpenModel)
+# if(dat_in_trial_i$Canopy1 == 1 | dat_in_trial_i$Can1aCOV > 0){
+#   Trial_i <- Trial_i %>%
+#     mutate(
+#       f1 = param$fZero,
+#       TotalTemp = cumsum(T_Stress),
+#       TotalTemp = replace(TotalTemp, TotalTemp > 990, 990),
+#       fTemp = TotalTemp * 0.001,
+#       fTemp = replace(fTemp, fTemp < 0.00001, 0.00001),
+#       f = 0.0015 + (0.99-0.0015)/(1+exp(-4*log((fTemp/(1-fTemp)))))
+#     )
+# }
 
 ## END LOOP HERE
 
