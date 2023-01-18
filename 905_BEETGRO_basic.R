d73ee7411b14a174f5015e1159e56e26cbcde719
Will English <80693248+meranbioph@users.noreply.github.com>
2023-01-18 09:28:31 +0100

    BEETGROW_basic = calc for trial i, with weather data coming from BEETGROW_weather.

--- 905_BEETGRO_basic.R
+++ 905_BEETGRO_basic.R
@@ -79,11 +79,11 @@
 # read in model parameters
 param <- pivot_wider(read_xlsx("parameters.xlsx", sheet = "parameters"),names_from = parameter)
 # read in trial data
-dat_in_trial <- read_xlsx("TrialData.xlsx", sheet = "TrialData")
-# read in weather data
-dat_in_weath <- read_xlsx("dat_weather_2021_40141.xlsx", sheet = "Sheet1")
+trial <- read_xlsx("TrialData.xlsx", sheet = "TrialData")
+# read in weather data FOR DEVELOPMENT. SYSTEM BELOW IS FOR NORMAL OPERATION.
+weath <- read_xlsx("dat_weather_2021_40141.xlsx", sheet = "Sheet1")
 # read which sites are in trail data, and read in appropriate weather data
-SiteIDs <- unique(dat_in_trial$SiteID)
+SiteIDs <- unique(trial$SiteID)
 Sites <- data.frame("year" = numeric(),
                     "doy" = numeric(),
                     "Tmax" = numeric(),
@@ -113,50 +113,43 @@ if(length(missing_files) == 0){
 }
 
 ############################################
-# INITIATE A SINGLE TRIAL "i"
+# INITIATE A SINGLE TRIAL "i" (TRIAL = SITE x YEAR)
+
 i=7L
 
 ## START LOOP HERE!
 
-dat_in_trial_i <- dat_in_trial[i,]
+trial_i <- trial[i,]
 
-b <- ifelse(dat_in_trial_i$SoilB < 1, 2.1, dat_in_trial_i$SoilB)
-pop1 <- ifelse(dat_in_trial_i$POP1 < 90000, -0.0003*(dat_in_trial_i$POP1/1000)^2+0.0456*(dat_in_trial_i$POP1/1000)-1.0246, 1)
-pop2 <- ifelse(dat_in_trial_i$POP2 < 90000, -0.0003*(dat_in_trial_i$POP2/1000)^2+0.0456*(dat_in_trial_i$POP2/1000)-1.0246, 1)
-pop3 <- ifelse(dat_in_trial_i$POP3 < 90000, -0.0003*(dat_in_trial_i$POP3/1000)^2+0.0456*(dat_in_trial_i$POP3/1000)-1.0246, 1)
-poploss <- ifelse(dat_in_trial_i$PlantPop == 0, 1, mean(pop1, pop2, pop3))
+b <- ifelse(trial_i$SoilB < 1, 2.1, trial_i$SoilB)
+pop1 <- ifelse(trial_i$POP1 < 90000, -0.0003*(trial_i$POP1/1000)^2+0.0456*(trial_i$POP1/1000)-1.0246, 1)
+pop2 <- ifelse(trial_i$POP2 < 90000, -0.0003*(trial_i$POP2/1000)^2+0.0456*(trial_i$POP2/1000)-1.0246, 1)
+pop3 <- ifelse(trial_i$POP3 < 90000, -0.0003*(trial_i$POP3/1000)^2+0.0456*(trial_i$POP3/1000)-1.0246, 1)
+poploss <- ifelse(trial_i$PlantPop == 0, 1, mean(pop1, pop2, pop3))
 
 ## Soil parameters, function of b
 qfc <- param$a2 * (param$a1/5)^(1/b)
 qpwp <- param$a2 * (param$a1/1500)^(1/b)
 
-dat_in_trial_i <- dat_in_trial_i %>%
-  mutate(
-    qfc = param$a2 * (param$a1/5)^(1/b),
-    qpwp = param$a2 * (param$a1/1500)^(1/b),
-    q = qfc,                                      # ISSUE: THIS MEANS THAT QREL (NEXT LINE) ALWAYS = 1.
-    
-  )
-
 ## Soil parameters, grouped by b
 if(b > 20){
-  dat_in_trial_i <- dat_in_trial_i %>%
+  trial_i <- trial_i %>%
     mutate(
       kappa = 0.0008,
       gamma_i = 0.00002701,
-      WstressA = 5,
-      WstressB = 0.8,
-      WstressC = 200,
+      w_stressA = 5,
+      w_stressB = 0.8,
+      w_stressC = 200,
       RUEZero1 = 2.1
   )
 } else {
-  dat_in_trial_i <- dat_in_trial_i %>%
+  trial_i <- trial_i %>%
     mutate(
       kappa = 0.0027,
       gamma_i = 0.00014,
-      WstressA = 2,
-      WstressB = 0.6,
-      WstressC = 300,
+      w_stressA = 2,
+      w_stressB = 0.6,
+      w_stressC = 300,
       RUEZero1 = param$RUEZero
   )
 }
@@ -164,15 +157,15 @@ if(b > 20){
 ############################################
 # TABLE OF EACH PARAMETER FOR EACH DAY OF YEAR FOR TRIAL "i".
 
-sow_date <- date(as.POSIXct(unlist(dat_in_trial_i["SowDate"]), origin = '1970-01-01'))
-emerg_date <- date(as.POSIXct(unlist(dat_in_trial[i,"EmDate"]), origin = '1970-01-01'))
-harvest_date <- date(as.POSIXct(unlist(dat_in_trial[i,"HarvestDate"]), origin = '1970-01-01'))
+sow_date <- date(as.POSIXct(unlist(trial_i["SowDate"]), origin = '1970-01-01'))
+emerg_date <- date(as.POSIXct(unlist(trial[i,"EmDate"]), origin = '1970-01-01'))
+harvest_date <- date(as.POSIXct(unlist(trial[i,"HarvestDate"]), origin = '1970-01-01'))
 sow_doy <-yday(sow_date)
 emerg_doy <-yday(emerg_date)
 harvest_doy <-yday(harvest_date)
 
-can_i <- dat_in_weath %>%
-  select(doy, xtemp, neda, et
+BG_i <- weath %>%
+  select(doy, xtemp, ned, et
          ) %>%
 
 # Crop status
@@ -192,7 +185,7 @@ can_i <- dat_in_weath %>%
   mutate(
     Cd_sow = cumsum(dT),
     Cd_sow = replace(Cd_sow, bbch == 00, 0),
-    bbch = replace(bbch, dat_in_trial_i$EmDOY <= dat_in_trial_i$SowDOY & Cd_sow >= param$Tzero, 01)
+    bbch = replace(bbch, trial_i$EmDOY <= trial_i$SowDOY & Cd_sow >= param$Tzero, 01)
   ) %>%
   group_by(emerged = bbch >= 09) %>%
   mutate(
@@ -202,64 +195,80 @@ can_i <- dat_in_weath %>%
   ungroup
 
 # GOING ROW WISE(). BUT THE PROBLEM IS THAT YOU CAN'T LAG IN ROWWISE(), SO HAVE TO GO TO LOOP
-for(j in 1:nrow(can_i)){
-  can_ij <- can_i[j,] %>%
+for(j in 1:(sow_doy-1)){
+  BG_ij <- BG_i[j,] %>%
+    mutate(smd = 20,
+           smd0 = 20,
+           qrel = 1,
+           f = param$fZero,
+           Cd_stress = 0,
+           SSE = 0)
+  
+  if(j==1) BG_j <- BG_ij
+  if(j!=1) BG_j <- bind_rows(BG_j, BG_ij)
+}
+for(j in sow_doy:nrow(BG_i)){
+  BG_ij <- BG_i[j,] %>%
     mutate(
-      # INITIAL CONDITIONS
+      # VALUES WITH LAGS
+      qrel = BG_j$qrel[j-1],
+      smd = BG_j$smd[j-1], 
+      smd0 = case_when(doy == sow_doy ~ 0, smd < 0 ~ 0, T ~ smd), # This is smd from previous period, with min = 0
+      SSE = BG_j$SSE[j-1],
       
-      # CANOPY
-      f = param$fZero,
-      WStress = 1,
-      WStress = replace(WStress, qrel < dat_in_trial_i$WstressB & xtemp > dat_in_trial_i$WstressC, 1),
-      T_Stress = (xtemp-3) * WStress, # Called DailyTemp in OpenModel
-      T_Stress = replace(T_Stress, xtemp < 3 | xtemp > 25 | bbch < 09, 0),
-      TotalTemp = cumsum(T_Stress),
-      TotalTemp = replace(TotalTemp, TotalTemp > 990, 990),
-      fTemp = TotalTemp * 0.001,
+      # CANOPY (f)
+      w_stress = ifelse(qrel < trial_i$w_stressB & xtemp > trial_i$w_stressC, (qrel/trial_i$w_stressB)*trial_i$Wstresc, 1),
+      xt_stress = ifelse(xtemp > 3 & xtemp < 25 & bbch >= 09, (xtemp-3) * w_stress, 0), # xtemp Called Weather.DailyTemp in OpenModel, xt_stress called DailyTemp in OpenModel
+      Cd_stress = unlist(BG_j[j-1,"Cd_stress"]) + xt_stress, # Cd_stress called TotalTemp in OpenModel
+      fTemp = Cd_stress * 0.001,
       fTemp = replace(fTemp, fTemp < 0.00001, 0.00001),
-      f = 0.0015 + (0.99-0.0015)/(1+exp(-4*log((fTemp/(1-fTemp))))),
-      
-      # ROOT DEPTH
+      Cd_stress = replace(Cd_stress, Cd_stress > 950, 950),
+      f = 0.0015 + (0.99-0.0015)/(1+exp(-4*log(fTemp/(1-fTemp)))),
+
+      # ROOT DEPTH (is sowing depth until emergence.)
       root_depth = param$Dsowing + param$length0*exp(param$beta0/param$delta*(1-exp(-param$delta*(Cd_em-param$Tzero)))),
       root_depth = replace(root_depth, bbch < 09, param$Dsowing),
-      
+
       # WATER RELATIONS
-      smd = 20,
-      smd = replace(smd, doy == sow_doy, 0),
-      smd = replace(smd, smd < 0, 0),
+      # SSE SOIL SURFACE EVAPORATION
       dSSE = ifelse(f < 1, min(1.5, et)*(1-f), 0),
+      dSSE = replace(dSSE, SSE > 20, 0),
+      SSE = SSE + dSSE - ned,
+      SSE = replace(SSE, SSE < 0, 0),
+      # ATMOSPHERE LIMITED CROP TRANSPIRATION
       eatmos = 1.2*f*et,
       eatmos = replace(eatmos, eatmos <= 0, 0.001),
-      q = max(qfc - smd/(root_depth*1000), 0.01),
+      # PARAMETERS
+      q = max(qfc - smd0/(root_depth*1000), 0.01), # depth is always greater than 0 post sowing, so if statement for q removed.
       qrel = q/qfc,
-      psiSoil = -5*(qrel)^(-b),
-      rsum = param$c1 + param$c2/root_depth*((qrel)^(-(2*b+3))-1)
+      psi_soil = -5*(qrel)^(-b),
+      rsum = param$c1 + param$c2/root_depth*(qrel^(-2*b+3)-1),
+      hc_soil = qrel^(2*b+3),
+      # SOIL LIMITED MAXIMUM CROP TRANSPIRATION
+      esoil = (psi_soil - param$psiCrop)/rsum,
+      rsum = replace(rsum, qrel == 0, param$c1),
+      esoil = replace(esoil, qrel == 0, 0),
+      # ACTUAL CROP EVAPOTRANSPIRATION
+      ea = min(eatmos, esoil),
+      # SOIL MOISTURE DEFICIT NB: IF THIS GIVE A NEGATIVE RESULT, 0 IS USED WHEN SMD IS USED TO CALC q IN THE NEXT DAY.
+      dsmd = dSSE + ea - ned,
+      smd = smd0 + dsmd
       )
   
-  if(j==1) can_j <- can_ij
-  if(j!=1) can_j <- bind_rows(can_j, can_ij)
-  }
+  BG_j <- bind_rows(BG_j, BG_ij)
+}
 
-can_i_names <- names(can_i)
-can_i_names <- can_i_names[-1]
-can_j <- can_j[,-which(names(can_j) %in% can_i_names)]
+BG_i_names <- names(BG_i)
+BG_i_names <- BG_i_names[-1]
+BG_j <- BG_j[,-which(names(BG_j) %in% BG_i_names)]
 
-can_i <- can_i %>% 
-  left_join(can_j, by ="doy")
+BG_i <- BG_i %>% 
+  left_join(BG_j, by ="doy")
 
-# # Canopy, with adjustments from user input (Pretty sure this isn't working properly in OpenModel)
-# if(dat_in_trial_i$Canopy1 == 1 | dat_in_trial_i$Can1aCOV > 0){
-#   Trial_i <- Trial_i %>%
-#     mutate(
-#       f1 = param$fZero,
-#       TotalTemp = cumsum(T_Stress),
-#       TotalTemp = replace(TotalTemp, TotalTemp > 990, 990),
-#       fTemp = TotalTemp * 0.001,
-#       fTemp = replace(fTemp, fTemp < 0.00001, 0.00001),
-#       f = 0.0015 + (0.99-0.0015)/(1+exp(-4*log((fTemp/(1-fTemp)))))
-#     )
-# }
+# SAVE OUTPUT FILE BY TRIAL (SITE x YEAR)
+write_xlsx(BG_i, paste0("BG_",trial_i$SiteID,"_",trial_i$Year,".xlsx"))
 
 ## END LOOP HERE
 
-write_xlsx(can_i, paste0("dat_canopy_2021.xlsx"))
+
+
